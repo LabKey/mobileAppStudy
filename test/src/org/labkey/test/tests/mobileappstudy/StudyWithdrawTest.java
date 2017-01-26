bfc06953adee89f0de562cda3023c7e0607b0ec4
labkey-ians <isigmon@users.noreply.github.com>
2017-01-26 00:40:25 -0800

    Spec Issue 28738: Allow participant to withdraw from study Test code review changes
    Added Enrollment tokens
    Fixed list assertions

--- test/src/org/labkey/test/tests/mobileappstudy/StudyWithdrawTest.java
+++ test/src/org/labkey/test/tests/mobileappstudy/StudyWithdrawTest.java
@@ -1,5 +1,6 @@
 package org.labkey.test.tests.mobileappstudy;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpResponse;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
@@ -11,8 +12,8 @@ import org.labkey.remoteapi.query.SelectRowsCommand;
 import org.labkey.remoteapi.query.SelectRowsResponse;
 import org.labkey.test.TestFileUtils;
 import org.labkey.test.categories.Git;
-import org.labkey.test.commands.mobileappstudy.SubmitResponseCommand;
 import org.labkey.test.commands.mobileappstudy.WithdrawParticipantCommand;
+import org.labkey.test.components.mobileappstudy.TokenBatchPopup;
 import org.labkey.test.data.mobileappstudy.AbstractQuestionResponse;
 import org.labkey.test.data.mobileappstudy.ChoiceQuestionResponse;
 import org.labkey.test.data.mobileappstudy.GroupedQuestionResponse;
@@ -21,17 +22,22 @@ import org.labkey.test.data.mobileappstudy.MedForm;
 import org.labkey.test.data.mobileappstudy.QuestionResponse;
 import org.labkey.test.data.mobileappstudy.Survey;
 import org.labkey.test.pages.mobileappstudy.SetupPage;
+import org.labkey.test.pages.mobileappstudy.TokenListPage;
 import org.labkey.test.util.ListHelper;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 /**
- * Created by RyanS on 1/23/2017.
+ * Test to verify participant withdrawal from a MobileAppStudy
  */
 @Category({Git.class})
 public class StudyWithdrawTest extends BaseMobileAppStudyTest
@@ -46,12 +52,14 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
             "\t\t\"results\": []\n" +
             "}";
     private static final List<String> TABLES = Arrays.asList("Response","ResponseMetadata","EnrollmentToken");
-    private static final List<String> LISTS = Arrays.asList("InitialSurvey","InitialSurveyBirthControlType",
-            "InitialSurveyOtc","InitialSurveyOtcMedCondition","InitialSurveyOtcMedName","InitialSurveyRx",
-            "InitialSurveyRxGroup","InitialSurveyRxMedCondition","InitialSurveyRxMedName","InitialSurveyRxMedVsRxUse",
-            "InitialSurveyRxWhyDifferent","InitialSurveySupplements");
+    //Lists populated by test data
+    private static final List<String> LISTS = Arrays.asList("InitialSurvey", "InitialSurveyRx", "InitialSurveyRxGroup",
+            "InitialSurveyRxMedName", "InitialSurveySupplements");
     private final static String SURVEY_NAME = "InitialSurvey";
     private final static String SURVEY_VERSION = "123.9";
+
+    private final static String ENROLLED = "0";
+
     private static String HAS_RESPONSES_DELETE;
     private static String HAS_RESPONSES_NO_DELETE;
     private static String NO_RESPONSES_DELETE;
@@ -74,16 +82,24 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
         setupPage.studySetupWebPart.setShortName(STUDY_NAME);
         setupPage.studySetupWebPart.checkResponseCollection();
         setupPage.studySetupWebPart.clickSubmit();
-        HAS_RESPONSES_DELETE = getNewAppToken(PROJECT_NAME, STUDY_NAME, null );
-        HAS_RESPONSES_NO_DELETE = getNewAppToken(PROJECT_NAME, STUDY_NAME, null );
-        NO_RESPONSES_DELETE = getNewAppToken(PROJECT_NAME, STUDY_NAME, null );
-        WITHDRAWS_TWICE = getNewAppToken(PROJECT_NAME, STUDY_NAME, null );
-        STAYS_IN = getNewAppToken(PROJECT_NAME, STUDY_NAME, null );
-        HRD_KEY = appTokenToParticipantId(HAS_RESPONSES_DELETE);
-        HRND_KEY = appTokenToParticipantId(HAS_RESPONSES_NO_DELETE);
-        NRD_KEY = appTokenToParticipantId(NO_RESPONSES_DELETE);
-        WT_KEY = appTokenToParticipantId(WITHDRAWS_TWICE);
-        SI_KEY = appTokenToParticipantId(STAYS_IN);
+
+        //Use enrollment tokens so EnrollmentToken Table is populated
+        TokenBatchPopup tokenBatchPopup = setupPage.tokenBatchesWebPart.openNewBatchPopup();
+        TokenListPage tokenListPage = tokenBatchPopup.createNewBatch("100");
+        int token = 0;
+        HAS_RESPONSES_DELETE = getNewAppToken(PROJECT_NAME, STUDY_NAME, tokenListPage.getToken(token++));
+        HAS_RESPONSES_NO_DELETE = getNewAppToken(PROJECT_NAME, STUDY_NAME, tokenListPage.getToken(token++));
+        NO_RESPONSES_DELETE = getNewAppToken(PROJECT_NAME, STUDY_NAME, tokenListPage.getToken(token++));
+        WITHDRAWS_TWICE = getNewAppToken(PROJECT_NAME, STUDY_NAME, tokenListPage.getToken(token++));
+        STAYS_IN = getNewAppToken(PROJECT_NAME, STUDY_NAME, tokenListPage.getToken(token++));
+
+        Map<String,String> appTokenParticipantIds = getColumnMap("Participant", "AppToken", "RowId");
+        HRD_KEY = appTokenParticipantIds.get(HAS_RESPONSES_DELETE);
+        HRND_KEY = appTokenParticipantIds.get(HAS_RESPONSES_NO_DELETE);
+        NRD_KEY = appTokenParticipantIds.get(NO_RESPONSES_DELETE);
+        WT_KEY = appTokenParticipantIds.get(WITHDRAWS_TWICE);
+        SI_KEY = appTokenParticipantIds.get(STAYS_IN);
+
         _listHelper.createList(getProjectName(), SURVEY_NAME, ListHelper.ListColumnType.AutoInteger, "Key" );
         setupLists();
         submitResponses(Arrays.asList(HAS_RESPONSES_DELETE,HAS_RESPONSES_NO_DELETE,WITHDRAWS_TWICE,STAYS_IN));
@@ -132,29 +148,54 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
         command = new WithdrawParticipantCommand(WITHDRAWS_TWICE, true, this::log);
         command.execute(400);
 
+        //*** Verify Participant Table ***
         //all users except STAYS_IN should no longer be enrolled
         log("verify participant status");
-        Assert.assertFalse("User " + HRD_KEY + " still shown as enrolled in Participant after withdrawing", isUserEnrolled(HRD_KEY));
-        Assert.assertFalse("User " + HRND_KEY + " still shown as enrolled in Participant after withdrawing", isUserEnrolled(HRND_KEY));
-        Assert.assertFalse("User " + NRD_KEY + " still shown as enrolled in Participant after withdrawing", isUserEnrolled(NRD_KEY));
-        Assert.assertFalse("User " + WT_KEY + " still shown as enrolled in Participant after withdrawing", isUserEnrolled(WT_KEY));
+        Map<String, String> colMap = getColumnMap("Participant", "RowId", "Status");
+        Assert.assertFalse("User " + HRD_KEY + " still shown as enrolled in Participant after withdrawing", ENROLLED.equals(colMap.get(HRD_KEY)));
+        Assert.assertFalse("User " + HRND_KEY + " still shown as enrolled in Participant after withdrawing", ENROLLED.equals(colMap.get(HRND_KEY)));
+        Assert.assertFalse("User " + NRD_KEY + " still shown as enrolled in Participant after withdrawing", ENROLLED.equals(colMap.get(NRD_KEY)));
+        Assert.assertFalse("User " + WT_KEY + " still shown as enrolled in Participant after withdrawing", ENROLLED.equals(colMap.get(WT_KEY)));
         //User who did not withdraw should be enrolled
-        Assert.assertTrue("User " + SI_KEY + " still shown as enrolled in Participant after withdrawing", isUserEnrolled(SI_KEY));
-        log("verify participant data deletion");
-        //Users who have elected to have data deleted should have responses deleted
-        Assert.assertTrue("Data found for user " + HRD_KEY + " that should have been deleted",tablesWithParticipantData(HRD_KEY).size()==0);
-        Assert.assertTrue("Data found for user " + NRD_KEY + " that should have been deleted", tablesWithParticipantData(NRD_KEY).size()==0);
-        //Users who have elected not to have data deleted should retain data
-        Assert.assertTrue("Data should have been retained for user " + HRND_KEY + " but was not",tablesWithParticipantData(HRND_KEY).size()>0);
-        Assert.assertTrue("Data should have been retained for user " + SI_KEY + " but was not",tablesWithParticipantData(SI_KEY).size()>0);
-        Assert.assertTrue("Data should have been retained for user " + WT_KEY + " but was not",tablesWithParticipantData(WT_KEY).size()>0);
+        Assert.assertTrue("User " + SI_KEY + " still shown as enrolled in Participant after withdrawing", ENROLLED.equals(colMap.get(SI_KEY)));
+
         //check tokens are null for withdrawn participants
-        Assert.assertTrue("User " + HRD_KEY + " did not have appToken null after withdrawal", isAppTokenNull(HAS_RESPONSES_DELETE));
-        Assert.assertTrue("User " + HRND_KEY + " did not have appToken null after withdrawal", isAppTokenNull(HAS_RESPONSES_NO_DELETE));
-        Assert.assertTrue("User " + NRD_KEY + " did not have appToken null after withdrawal", isAppTokenNull(NO_RESPONSES_DELETE));
-        Assert.assertTrue("User " + WT_KEY + " did not have appToken null after withdrawal", isAppTokenNull(WITHDRAWS_TWICE));
+        colMap = getColumnMap("Participant", "RowId", "AppToken");
+        Assert.assertTrue("User " + HRD_KEY + " did not have appToken null after withdrawal", StringUtils.isBlank(colMap.get(HRD_KEY)));
+        Assert.assertTrue("User " + HRND_KEY + " did not have appToken null after withdrawal", StringUtils.isBlank(colMap.get(HRND_KEY)));
+        Assert.assertTrue("User " + NRD_KEY + " did not have appToken null after withdrawal", StringUtils.isBlank(colMap.get(NRD_KEY)));
+        Assert.assertTrue("User " + WT_KEY + " did not have appToken null after withdrawal", StringUtils.isBlank(colMap.get(WT_KEY)));
         //check tokens retained for remaining users
-        Assert.assertFalse("User " + SI_KEY + " did not withdraw but did not retain appToken ", isAppTokenNull(STAYS_IN));
+        Assert.assertFalse("User " + SI_KEY + " did not withdraw but did not retain appToken ", StringUtils.isBlank(colMap.get(SI_KEY)));
+
+
+        //*** Verify Other Tables ***
+        String deletedFormat = "Data found for user %1$s in table %2$s that should have been deleted";
+        String retainedFormat = "Data should have been retained in table %2$s for user %1$s but was not";
+        log("verify participant data deletion from tables");
+        assertParticipantIds(MOBILEAPP_SCHEMA, TABLES, deletedFormat, retainedFormat);
+
+        //*** Verify Lists ***
+        deletedFormat = "Data found for user %1$s in list %2$s that should have been deleted";
+        retainedFormat = "Data should have been retained in list %2$s for user %1$s but was not";
+        log("verify participant data deletion from lists");
+        assertParticipantIds(LIST_SCHEMA, LISTS, deletedFormat, retainedFormat);
+    }
+
+    private void assertParticipantIds(String schema, List<String> tables, String deletedFormat, String retainedFormat)
+    {
+        for (String table : tables)
+        {
+            Set<String> participantIds = getParticipantIds(schema, table);
+
+            //Users who have elected to have data deleted should have responses deleted
+            Assert.assertFalse(String.format(deletedFormat, HRD_KEY, table), participantIds.contains(HRD_KEY));
+            Assert.assertFalse(String.format(deletedFormat, NRD_KEY, table), participantIds.contains(NRD_KEY));
+            //Users who have elected not to have data deleted should retain data
+            Assert.assertTrue(String.format(retainedFormat, HRND_KEY, table), participantIds.contains(HRND_KEY));
+            Assert.assertTrue(String.format(retainedFormat, SI_KEY, table), participantIds.contains(SI_KEY));
+            Assert.assertTrue(String.format(retainedFormat, WT_KEY, table), participantIds.contains(WT_KEY));
+        }
     }
 
     private SelectRowsResponse getMobileAppData(String table)
@@ -201,7 +242,7 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
 
             type = AbstractQuestionResponse.SupportedResultType.TEXT;
             fieldName = InitialSurvey.ILLNESS_WEEK;
-            String val = "I \u9825 waffles";
+            String val = "I \u9829 waffles";
 
             qr = new QuestionResponse(type, fieldName, new Date(), new Date(), true, val);
             submitQuestion(qr, appToken, 200);
@@ -233,13 +274,6 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
         }
     }
 
-    /**
-     * Wrap question response and submit to server via the API
-     *
-     * @param qr to send to server
-     * @param appToken to use in request
-     * @return error message of request if there is one.
-     */
     private String submitQuestion(QuestionResponse qr, String appToken, int expectedStatusCode)
     {
         Survey survey = new InitialSurvey(appToken, SURVEY_NAME, SURVEY_VERSION, new Date(), new Date());
@@ -248,62 +282,18 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
         return submitSurvey(survey, expectedStatusCode);
     }
 
-    /**
-     * Submit the survey to server via API
-     *
-     * @param survey to submit
-     * @param expectedStatusCode status code to expect from server
-     * @return error message from response (if it exists)
-     */
-    private String submitSurvey(Survey survey, int expectedStatusCode)
+    private Map<String, String> getColumnMap(String table, String keyCol, String valCol)
     {
-        SubmitResponseCommand cmd = new SubmitResponseCommand(this::log, survey);
-        cmd.execute(expectedStatusCode);
-
-        return cmd.getExceptionMessage();
+        Map<String, String> map = new HashMap<>();
+        getMobileAppData(table).getRows().forEach(row -> map.put(String.valueOf(row.get(keyCol)), Objects.toString(row.get(valCol), "")));
+        return map;
     }
 
-    private boolean isUserEnrolled(String participantId)
+    private Set<String> getParticipantIds(String schema, String tableName)
     {
-        SelectRowsResponse selectResponse = getMobileAppData("Participant");
-        List<Map<String,Object>> rows = selectResponse.getRows();
-        for(Map<String,Object> row : rows)
-        {
-            if(String.valueOf(row.get("RowId")).equals(participantId))
-            {
-                if(row.get("Status").equals(0)) return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isAppTokenNull(String appToken)
-    {
-        SelectRowsResponse selectResponse = getMobileAppData("Participant");
-        List<Map<String,Object>> rows = selectResponse.getRows();
-        for(Map<String,Object> row : rows)
-        {
-            if(String.valueOf(row.get("AppToken")).equals(appToken))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private String appTokenToParticipantId(String appToken)
-    {
-        SelectRowsResponse selectResponse = getMobileAppData("Participant");
-        List<Map<String,Object>> rows = selectResponse.getRows();
-        String participant = "";
-        for(Map<String,Object> row : rows)
-        {
-            if(row.get("AppToken").equals(appToken))
-            {
-                participant=String.valueOf(row.get("RowId"));
-            }
-        }
-        return participant;
+        HashSet<String> set = new HashSet<>();
+        getMobileAppData(tableName, schema).getRows().forEach(row -> set.add(Objects.toString(row.get("ParticipantId"), "")));
+        return set;
     }
 
     private List<String> getCol(String table, String col, String schema)
@@ -322,19 +312,5 @@ public class StudyWithdrawTest extends BaseMobileAppStudyTest
     {
         List<String> col = getCol(table, "ParticipantId",schema);
         return col.contains(participantId);
-    }
-
-    private List<String> tablesWithParticipantData(String participantId)
-    {
-        List<String> tablesWithData = new ArrayList<>();
-        for(String table : TABLES)
-        {
-            if(isParticipantIdPresent(table, participantId,MOBILEAPP_SCHEMA))
-            { tablesWithData.add(table);}
-        }
-        for(String table : LISTS)
-            if(isParticipantIdPresent(table, participantId,LIST_SCHEMA))
-            { tablesWithData.add(table);}
-        return tablesWithData;
     }
 }
