134edd2ed8e75c5df0c67128395ef335edae12b6
labkey-ians <isigmon@users.noreply.github.com>
2017-01-31 14:20:27 -0800

    Issue 29202: Validate enrollment token action Added action and test

---
+++ test/src/org/labkey/test/commands/mobileappstudy/EnrollmentTokenValidationCommand.java
@@ -0,0 +1,84 @@
+package org.labkey.test.commands.mobileappstudy;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.labkey.test.WebTestHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+
+public class EnrollmentTokenValidationCommand extends MobileAppCommand
+{
+    protected static final String CONTROLLER_NAME = "mobileappstudy";
+    protected static final String ACTION_NAME = "validateenrollmenttoken";
+    public static final String INVLAID_STUDYID_FORMAT = "Study with StudyId '%1$s' does not exist";
+    public static final String INVALID_TOKEN_FORMAT = "Invalid token: '%1$s'";
+    public static final String BLANK_STUDYID = "StudyId is required for enrollment";
+    public static final String TOKEN_REQUIRED = "Token is required for enrollment";
+
+    private String _batchToken;
+    private String _studyName;
+    private String _projectName;
+
+    public String getProjectName()
+    {
+        return _projectName;
+    }
+    public void setProjectName(String projectName)
+    {
+        _projectName = projectName;
+    }
+
+    public EnrollmentTokenValidationCommand(String project, String studyName, String batchToken, Consumer<String> logger)
+    {
+        _studyName = studyName;
+        _batchToken = batchToken;
+        _projectName = project;
+
+        setLogger(logger);
+    }
+
+    public String getStudyName()
+    {
+        return _studyName;
+    }
+    public void setStudyName(String studyName)
+    {
+        _studyName = studyName;
+    }
+
+    public String getBatchToken()
+    {
+        return _batchToken;
+    }
+    public void setBatchToken(String batchToken)
+    {
+        _batchToken = batchToken;
+    }
+
+    @Override
+    public HttpResponse execute(int expectedStatusCode)
+    {
+        HttpPost post = new HttpPost(getTargetURL());
+        return execute(post, expectedStatusCode);
+    }
+
+    @Override
+    public String getTargetURL()
+    {
+        Map<String, String> params = new HashMap<>();
+        params.put("studyId", getStudyName());
+        if (StringUtils.isNotBlank(getBatchToken()))
+            params.put("token", getBatchToken());
+        return WebTestHelper.buildURL(CONTROLLER_NAME, getProjectName(), ACTION_NAME, params);
+    }
+
+    @Override
+    public String getBody()
+    {
+        return "";
+    }
+}
