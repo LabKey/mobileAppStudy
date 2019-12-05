3d27611ee8379d7cb9512ea83905129d8ac0af70
ian <ians@labkey.com>
2019-12-05 13:47:36 -0800

    Post ParticipantProperties confirmation changes - Remove the '/' also

--- src/org/labkey/mobileappstudy/surveydesign/ServiceSurveyDesignProvider.java
+++ src/org/labkey/mobileappstudy/surveydesign/ServiceSurveyDesignProvider.java
@@ -111,8 +111,8 @@ public class ServiceSurveyDesignProvider extends AbstractSurveyDesignProviderImp
         value = value.trim();
 
         //Allow backwards compatibility to baseUrl parameter, truncate /activity from the configured url if present
-        return StringUtils.endsWithIgnoreCase(value, ACTIVITY_ACTION) ?
-                value.substring(0, value.length() - ACTIVITY_ACTION.length()):
+        return StringUtils.endsWithIgnoreCase(value, "/" + ACTIVITY_ACTION) ?
+                value.substring(0, value.length() - ACTIVITY_ACTION.length() - 1): //Truncate the action and the preceding '/'
                 value;
     }
 
